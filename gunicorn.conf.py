ba9e2e9243cbfc8b8aa856eb5e7e833db40fc19c
roy030407 <101629448+roy030407@users.noreply.github.com>
2025-06-13 16:48:44 +0400

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,8 @@
 import multiprocessing
+import os
 
-bind = "0.0.0.0:10000"
+port = int(os.getenv('PORT', 10000))
+bind = f"0.0.0.0:{port}"
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 120
 keepalive = 5
